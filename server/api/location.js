630f5ea619aacb4c41b9975b3aca023e6b75c84c
koogordo <koo.m.gordon@gmail.com>
2018-10-19 11:33:01 -0500

    starting create location route

--- server/api/location.js
+++ server/api/location.js
@@ -3,16 +3,45 @@ const router = express.Router();
 const mongo = require('mongoose');
 const Location = require('../models/Location');
 
+// GET
+// fetch a location doc by a city id
 router.get('/fetch/:id', (req, res) => {
+   errors = {}
   Location.findOne({ cityId: req.params.id }).then(loc => {
-    res.json(loc);
+     if(res) {
+      res.json(loc);
+     } else {
+        errors.location = `location with the id ${req.params.id} could'nt be found`
+        res.status(400).json(errors);
+     }
+    
+  }).catch(err => {
+     console.log(err);
   });
 });
 
+// GET
+// fetch all location docs
 router.get('/all', (req, res) => {
+  errors = {}
   Location.find().then(locs => {
-    res.json(locs);
+     if (locs) {
+      res.json(locs);
+     } else {
+        errors.allLocations = 'Could not fetch all locations';
+     }
+    
+  }).catch(err => {
+     console.log(err)
   });
 });
+
+// POST
+// add a new location
+router.post('/createlocation', (req, res) => {
+   if (req.body) {
+      Location.save()
+   }
+})
 
 module.exports = router;
