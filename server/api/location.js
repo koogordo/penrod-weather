446d2a613e865d99ddbb393410ca37f4d6242cc8
Gordon <ko2761oe@ko2761oem8.workstations.winona.edu>
2018-10-22 08:39:34 -0500

    more code cleaning

--- server/api/location.js
+++ server/api/location.js
@@ -6,16 +6,15 @@ const Location = require('../models/Location');
 // GET
 // fetch a location doc by a city id
 router.get('/fetch/:id', (req, res) => {
-  errors = {};
   Location.findOne({ cityId: req.params.id })
     .then(loc => {
       if (res) {
-        res.json(loc);
+        return res.json({ success: true, message: loc });
       } else {
-        errors.location = `location with the id ${
-          req.params.id
-        } could'nt be found`;
-        res.status(400).json(errors);
+        return res.json({
+          success: false,
+          message: `location with the id ${req.params.id} couldn't be found`
+        });
       }
     })
     .catch(err => {
@@ -26,13 +25,15 @@ router.get('/fetch/:id', (req, res) => {
 // GET
 // fetch all location docs
 router.get('/all', (req, res) => {
-  errors = {};
   Location.find()
     .then(locs => {
       if (locs) {
         res.json(locs);
       } else {
-        errors.allLocations = 'Could not fetch all locations';
+        return res.json({
+          success: false,
+          message: 'Coud not fetch any documents'
+        });
       }
     })
     .catch(err => {
